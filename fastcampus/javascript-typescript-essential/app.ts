38fc6ec8150d7f10e38a3fd0c4e115e5c59496ef
정수진 <sssjsjj@gmail.com>
2023-01-31 18:09:33 +0900

    generic적용과 함수 타입 적용 마무리

--- fastcampus/javascript-typescript-essential/app.ts
+++ fastcampus/javascript-typescript-essential/app.ts
@@ -21,11 +21,11 @@ type NewsFeed = News & {
 
 type NewsDetail = News & {
   contents_count: number;
-  comments?: NewsComment[];
+  comments: NewsComment[];
 }
 
 type NewsComment = News & {
-  comments?: NewsComment[];
+  comments: NewsComment[];
   level: number;
 }
 
@@ -38,14 +38,14 @@ const store: Store = {
   feeds: [],
 };
 
-function getData(url: string): NewsFeed[] | NewsDetail[] {
+function getData<AjaxResponse>(url: string): AjaxResponse {
   ajax.open('GET', url, false);
   ajax.send();
 
   return JSON.parse(ajax.response);
 }
 
-function makeFeeds(feeds: []) {
+function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
   for (let i = 0; i < feeds.length; i++) {
     feeds[i].read = false;
   }
@@ -53,7 +53,14 @@ function makeFeeds(feeds: []) {
   return feeds;
 }
 
-function newsFeed() {
+function updateView(html: string): void { //return 값이 없을때
+  if(container) 
+    container.innerHTML = html
+  else
+    console.error('최상위 컨테이너가 없어 UI를 삽입할 수 없습니다.')
+}
+
+function newsFeed(): void {
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
@@ -82,7 +89,7 @@ function newsFeed() {
   `;
 
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL))
   }
 
   for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -108,15 +115,15 @@ function newsFeed() {
   }
 
   template = template.replace('{{__news_feed__}}', newsList.join(''));
-  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-  template = template.replace('{{__next_page__}}', store.currentPage + 1);
+  template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+  template = template.replace('{{__next_page__}}', String(store.currentPage + 1));
   
   updateView(template)
 }
 
-function newsDetail() {
+function newsDetail(): void {
   const id = location.hash.substr(7);
-  const newsContent = getData(CONTENT_URL.replace('@id', id))
+  const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id))
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -153,41 +160,35 @@ function newsDetail() {
     }
   }
 
-  function makeComment(comments, called = 0) {
-    const commentString = [];
-
-    for(let i = 0; i < comments.length; i++) {
-      commentString.push(`
-        <div style="padding-left: ${called * 40}px;" class="mt-4">
-          <div class="text-gray-400">
-            <i class="fa fa-sort-up mr-2"></i>
-            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-          </div>
-          <p class="text-gray-700">${comments[i].content}</p>
-        </div>      
-      `);
-
-      if (comments[i].comments.length > 0) {
-        commentString.push(makeComment(comments[i].comments, called + 1));
-      }
-    }
-
-    return commentString.join('');
-  }
-
-  template = 
-  template.replace('{{__comments__}}', makeComment(newsContent.comments))
+  template = template.replace('{{__comments__}}', makeComment(newsContent.comments))
   updateView(template)
 }
 
-function updateView(html: string) {
-  if(container) 
-    container.innerHTML = html
-  else
-    console.error('최상위 컨테이너가 없어 UI를 삽입할 수 없습니다.')
+function makeComment(comments: NewsComment[]): string {
+  const commentString = [];
+
+  for(let i = 0; i < comments.length; i++) {
+    const { level, user, time_ago, content, comments: commentList }: NewsComment = comments[i]
+    
+    commentString.push(`
+      <div style="padding-left: ${level * 40}px;" class="mt-4">
+        <div class="text-gray-400">
+          <i class="fa fa-sort-up mr-2"></i>
+          <strong>${user}</strong> ${time_ago}
+        </div>
+        <p class="text-gray-700">${content}</p>
+      </div>      
+    `);
+
+    if (comments.length > 0) {
+      commentString.push(makeComment(commentList));
+    }
+  }
+
+  return commentString.join('');
 }
 
-function router() {
+function router(): void {
   const routePath = location.hash;
 
   if (routePath === '') {
