c32f7396d9e5a5d89657bb6c30de5f82921c6233
정수진 <sssjsjj@gmail.com>
2023-01-31 17:35:02 +0900

    변수 타입 작성

--- fastcampus/javascript-typescript-essential/app.ts
+++ fastcampus/javascript-typescript-essential/app.ts
@@ -1,13 +1,29 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+}
+
+type NewsFeed = {
+  id: number;
+  comments_count: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean; //선택속성 이라는 듯
+}
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
 
-function getData(url) {
+function getData(url: string) {
   ajax.open('GET', url, false);
   ajax.send();
 
@@ -23,7 +39,7 @@ function makeFeeds(feeds) {
 }
 
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -80,7 +96,7 @@ function newsFeed() {
   template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
   template = template.replace('{{__next_page__}}', store.currentPage + 1);
   
-  container.innerHTML = template;
+  updateView(template)
 }
 
 function newsDetail() {
@@ -144,7 +160,16 @@ function newsDetail() {
     return commentString.join('');
   }
 
-  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+  template = 
+  template.replace('{{__comments__}}', makeComment(newsContent.comments))
+  updateView(template)
+}
+
+function updateView(html: string) {
+  if(container) 
+    container.innerHTML = html
+  else
+    console.error('최상위 컨테이너가 없어 UI를 삽입할 수 없습니다.')
 }
 
 function router() {
