0bf2d12bcc1922c36bc115c4230d70b29d20e7ed
정수진 <sssjsjj@gmail.com>
2023-02-02 17:26:03 +0900

    xhr을 promise로 변경

--- fastcampus/javascript-typescript-essential/src/core/api.ts
+++ fastcampus/javascript-typescript-essential/src/core/api.ts
@@ -1,21 +1,30 @@
 import { NewsFeed, NewsDetail } from '../types'
 
 export class Api {
-  ajax: XMLHttpRequest
+  xhr: XMLHttpRequest
   url: string
 
   constructor(url: string) {
-    this.ajax = new XMLHttpRequest()
+    this.xhr = new XMLHttpRequest()
     this.url = url
   }
 
   // 바깥쪽에서 호출하지 않는것은 protected
-  getRequest<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
-    this.ajax.open('GET', this.url)
-    this.ajax.addEventListener('load', () => {
-      cb(JSON.parse(this.ajax.response) as AjaxResponse)
+  getRequestWithXHR<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
+    this.xhr.open('GET', this.url)
+    this.xhr.addEventListener('load', () => {
+      cb(JSON.parse(this.xhr.response) as AjaxResponse)
     })
-    this.ajax.send()
+    this.xhr.send()
+  }
+
+  getRequestWithPromise<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
+    fetch(this.url)
+      .then(res => res.json())
+      .then(cb)
+      .catch(() => {
+        console.error('데이터를 불러오지 못했습니다.')
+      })
   }
 }
 
@@ -24,8 +33,11 @@ export class NewsFeedApi extends Api {
     super(url)
   }
 
-  getData(cb: (data: NewsFeed[]) => void): void {
-    return this.getRequest<NewsFeed[]>(cb)
+  getDataWithXHR(cb: (data: NewsFeed[]) => void): void {
+    return this.getRequestWithPromise<NewsFeed[]>(cb)
+  }
+  getDataWithPromise(cb: (data: NewsFeed[]) => void): void {
+    return this.getRequestWithXHR<NewsFeed[]>(cb)
   }
 }
 
@@ -34,8 +46,11 @@ export class NewsDetailApi extends Api {
     super(url)
   }
 
-  getData(cb: (data: NewsDetail) => void): void {
-    return this.getRequest<NewsDetail>(cb)
+  getDataWithXHR(cb: (data: NewsDetail) => void): void {
+    return this.getRequestWithXHR<NewsDetail>(cb)
+  }
+  getDataWithPromise(cb: (data: NewsDetail) => void): void {
+    return this.getRequestWithPromise<NewsDetail>(cb)
   }
 }
 
--- fastcampus/javascript-typescript-essential/src/page/news-detail-view.ts
+++ fastcampus/javascript-typescript-essential/src/page/news-detail-view.ts
@@ -44,7 +44,7 @@ export default class NewsDetailView extends View {
   render(): void {
     const id = location.hash.substr(7);
     const api = new NewsDetailApi(CONTENT_URL.replace('@id', id))
-    api.getData((data: NewsDetail) => {
+    api.getDataWithPromise((data: NewsDetail) => {
       const { title, content, comments } = data
     
       this.store.makeRead(Number(id))
